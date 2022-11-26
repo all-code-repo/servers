795c523f3255406bad03d32c3b14fd4e39ccaa0a
090code <119131751+090code@users.noreply.github.com>
2022-11-27 01:05:38 +0530

    node_server modification
    added fake data with endpoint

--- node_server/app.js
+++ node_server/app.js
@@ -1,7 +1,26 @@
 const express = require('express')
+const { faker } = require('@faker-js/faker')
+const { application, json } = require('express')
+
 const app = express()
 const port = 3000
 
+app.get('/v1/employees', (req, res) => {
+  let response = [];
+  for(let i=0; i < 10; i++) {
+    response.push({
+      'name': faker.name.fullName(),
+      'sex': faker.name.sex(),
+      'dob': faker.date.birthdate(),
+      'married': faker.datatype.boolean(),
+      'jobTitle': faker.name.jobTitle(),
+      'address': faker.address.city(),
+      'company': faker.company.name(),
+      'email': faker.internet.email()
+    })
+  } 
+  res.send({data: response})
+})
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
